d61311bdb82986188fb6ee0a2da345ca7c67430e
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-02 16:43:41 +0900

    fix: 권한 처리

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ function App() {
   const cookie = new Cookies();
   const accessToken = cookie.get('access_token');
   const refreshToken = cookie.get('refresh_token');
+  const authority = cookie.get('authority');
   useEffect(() => {
     if (!accessToken || !refreshToken) {
       alert('로그인 후 이용 가능합니다');
       window.location.href = 'https://auth.entrydsm.hs.kr/admin-login?redirect_url=https://admin.entrydsm.hs.kr';
     }
-  }, [accessToken, refreshToken]);
+    if (authority != 'admin') {
+      alert('권한이 없습니다.');
+      window.location.href = 'https://auth.entrydsm.hs.kr/admin-login?redirect_url=https://admin.entrydsm.hs.kr';
+    }
+  }, [accessToken, refreshToken, authority]);
 
   return (
     <>
